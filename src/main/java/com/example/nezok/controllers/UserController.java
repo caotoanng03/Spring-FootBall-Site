683b02b6cbdfbfba7de0a86a712cd4591c95ef97
CaoToan <caotoan940@gmail.com>
2023-11-18 20:39:59 +0100

    Fix homepage content

--- src/main/java/com/example/nezok/controllers/UserController.java
+++ src/main/java/com/example/nezok/controllers/UserController.java
@@ -3,9 +3,14 @@ package com.example.nezok.controllers;
 import com.example.nezok.repositories.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
+    @GetMapping("/home")
+    public String user() {
+        return "/pages/user/user";
+    }
 
 }
